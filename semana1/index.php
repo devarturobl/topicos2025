c669fe4b666e3fde5f79e6f4c7eb269a15ad8b08
devarturobl <4rthurbl00@gmail.com>
2025-01-28 12:01:58 -0600

    Primera Clase

---
+++ semana1/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Semana 1 Topicos Av Intro PHP</title>
+</head>
+<body>
+    <h1>Hola este es el primer archivo de php</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet voluptate, harum doloremque laborum vitae deleniti itaque modi qui eveniet cupiditate illo. Voluptatem at eaque quisquam, veniam dignissimos harum pariatur dolores?</p>
+
+    <form method="post" action="">
+        <label for="num1">Número 1:</label>
+        <input type="number" id="num1" name="a" required>
+        <br>
+        <label for="num2">Número 2:</label>
+        <input type="number" id="num2" name="b" required>
+        <br>
+        <input type="submit" value="Sumar">
+    </form>
+
+    <?php
+        //Declarar Variable Regla 1 Poner el signo de $ antes del nombre
+        $saludo = "Hola Chicos esto es PHP";
+        echo "Hola Mundo, ".$saludo;
+
+        //Declarar una funcion en PHP
+        function suma($num1, $num2){
+            return $num1 + $num2;
+        }
+
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $num1 = $_POST["a"];
+            $num2 = $_POST["b"];
+            $resultado = suma($num1, $num2);
+            echo "<h2>El resultado de la suma es: $resultado</h2>";
+        }
+
+    ?>
+</body>
+</html>
