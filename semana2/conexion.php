5cd932a1ebff8bc7648e58e6707bf20929eac2d8
devarturobl <4rthurbl00@gmail.com>
2025-02-04 12:29:14 -0600

    semana2

---
+++ semana2/conexion.php
@@ -0,0 +1,10 @@
+<?php
+//Parametros del servidor local
+$localhost = "localhost";
+$user = "root";
+$password = "";
+$db = "demo1";
+
+//Variable de conexion al servidor
+$conexion = mysqli_connect($localhost, $user, $password, $db);
+?>
---
+++ semana2/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Semana 2 Topicos Avanzados de Programación</title>
+</head>
+<body>
+    <h1>Diseño de Operaciones CRUD en PHP y MySQli</h1>
+    <h2>Modulo 1: Insertar Datos</h2>
+    <h3>Formulario HTML a PHP</h3>
+
+    <form method = "post">
+        <p>Nombre: <input type="text" name="nombre"></p>
+        <p>Edad: <input type="text" name="edad"></p>
+        <p>Telefono: <input type="text" name="tel"></p>
+        <p><input type="submit" value="Enviar"></p>
+    </form>
+<!--Importando Conexion a base de datos-->
+    <?php
+        Include 'conexion.php';
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $name = $_POST['nombre'];
+           $age = $_POST['edad'];
+           $phone = $_POST['tel'];
+           //Metodo Insert
+           $query = mysqli_query($conexion, "INSERT INTO datos (nombre, edad, telefono) VALUES ('$name', '$age', '$phone')");
+        }
+    ?>
+</body>
+</html>
