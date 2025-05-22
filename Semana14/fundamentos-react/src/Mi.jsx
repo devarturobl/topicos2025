954a1ab6e5370c9e8244290d99df2dc4ff536d69
devarturobl <4rthurbl00@gmail.com>
2025-05-22 10:02:47 -0600

    Semana15

---
+++ Semana14/fundamentos-react/src/Mi.jsx
@@ -0,0 +1,10 @@
+const MiComponente = () => {  
+  return (
+    <div>
+      <h2>Mi Componente</h2>
+      <p>Este es un componente de ejemplo.</p>
+    </div>
+  );
+}
+
+export default MiComponente;
--- Semana15/practica1/src/MiContenido.jsx
+++ Semana15/practica1/src/MiContenido.jsx
@@ -1,5 +1,32 @@
 import { Fragment } from "react";
 
+const MyButton = () => {
+    const handleClick = () => {
+        alert("Hola, has hecho click en el botón");
+    }
+
+    return (
+        <button onClick={handleClick}>Haz click aquí</button>
+    );
+}
+
+const EsVerdad = () => {
+    return(
+    <div>
+        <img src="https://ajalpan.gob.mx/media/municipio/1636658845.jpg" alt="Placeholder" height={500} width={500}/>
+        <p>Esto es verdadero</p>
+    </div>
+    );
+}
+
+const EsFalso = () => {
+    return(
+    <div>
+        <img src="https://ajalpan.gob.mx/media/news/1747722803.jpg" alt="Placeholder" height={500} width={500}/>
+        <p>Esto es falso</p>
+    </div>
+    );
+}
 const MiContenido = () => {
     const nombre = "Juanito Perez";
     const pais = {
@@ -8,10 +35,23 @@ const MiContenido = () => {
         poblacion: 47000000,
         capital: "Madrid"
     };
+    const user = true;
+    const emojics = ["😡", "😠", "😤", "😨", "😩", "😫", "🥵"];
+
     return (
         <Fragment>
             <p>Lorem {nombre} {pais.nombre} {pais.continente} ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt incidunt eligendi inventore corrupti, amet vero, cupiditate tempora necessitatibus provident repudiandae esse magni doloribus voluptatem suscipit officia neque quia culpa?</p>
             <img src="https://thispersondoesnotexist.com/" alt="Placeholder" height={500} width={500}/>
+            <br></br>
+            <MyButton />
+            {/*user ? <EsVerdad/> : <EsFalso/> usando true y false el de abajo solo true*/}
+            {user && <EsVerdad/>}
+            <lu>
+                {emojics.map((emoji) => (
+                <li>{emoji}</li>
+                ))}
+            </lu>
+            
         </Fragment>
     );
 }
